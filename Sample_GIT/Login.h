b8720092ce175e1d9c786f2ce9822f6dfbf3749f
Luis <cd>
2016-10-20 16:57:28 -0400

    Created Structure for Login

---
+++ Sample_GIT/Login.h
@@ -0,0 +1,13 @@
+//
+//  Login.h
+//  Sample_GIT
+//
+//  Created by josh on 10/20/16.
+//  Copyright © 2016 MCS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Login : NSObject
+
+@end
--- Sample_GIT/ViewController.m
+++ Sample_GIT/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     
     NSLog(@"Hello Git");
+    NSLog(@"I am in Login");
 }
 
 
